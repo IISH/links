ff2c343be6511973362a61e96588ab1b1d4811cb
Fons Laan <fons.laan@gmail.com>
2015-11-11 14:40:17 +0100

    cosmetic

--- LinksProject/src/main/java/dataset/MinMaxYearSet.java
+++ LinksProject/src/main/java/dataset/MinMaxYearSet.java
@@ -1,26 +1,23 @@
 package dataset;
 
 /**
- * MinMaxJaar dataset
- * Used to return a min and an max Year
+ * MinMaxYear class
+ * Used to return a min and and max Year
  *
  * @author Omar Azouguagh
  * @author Fons Laan
  *
- * FL-16-Oct-2014 Latest change
+ * FL-10-Nov-2015 Latest change
  */
-public class MinMaxYearSet {
-
-    // Private vars
+public class MinMaxYearSet
+{
     private int minYear ;
     private int maxYear ;
-
 
     /**
      * @return minYear
      */
     public int getMinYear( ) { return this.minYear ; }
-
 
 
     /**
@@ -29,17 +26,14 @@ public class MinMaxYearSet {
     public int getMaxYear( ) { return this.maxYear ; }
 
 
-
     /**
      * @param minYear
      */
     public void setMinYear( int minYear ) { this.minYear = minYear ; }
 
 
-
     /**
      * @param maxYear
      */
     public void setMaxYear( int maxYear ) { this.maxYear = maxYear ; }
-
 }
