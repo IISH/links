631d96b565dd047fd0d46330ddce26440d000a82
Fons Laan <fons.laan@gmail.com>
2016-04-12 16:54:16 +0200

    timestamp

--- LinksProject/src/main/java/linksmanager/ManagerGui.java
+++ LinksProject/src/main/java/linksmanager/ManagerGui.java
@@ -65,7 +65,7 @@ import general.PrintLogger;
  * FL-29-Jul-2014 Explicit imports
  * FL-05-Aug-2014 ref db also in GUI
  * FL-20-Aug-2014 Occupation added
- * FL-07-Apr-2016 Latest change
+ * FL-12-Apr-2016 Latest change
  */
 
 public class ManagerGui extends javax.swing.JFrame
@@ -2995,7 +2995,7 @@ public class ManagerGui extends javax.swing.JFrame
         {
             public void run()
             {
-                String timestamp1 = "07-Apr-2016 09:27";
+                String timestamp1 = "12-Apr-2016 15:06";
                 String timestamp2 = LinksSpecific.getTimeStamp2( "yyyy.MM.dd-HH:mm:ss" );
 
                 try {
