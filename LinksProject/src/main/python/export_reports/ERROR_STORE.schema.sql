c91e84920adaee1dc35702955611c890b8c7a86c
Fons Laan <fons.laan@gmail.com>
2016-09-07 16:54:58 +0200

    reports

---
+++ LinksProject/src/main/python/export_reports/ERROR_STORE.schema.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.14  Distrib 5.5.50-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: links_logs
+-- ------------------------------------------------------
+-- Server version	5.5.50-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ERROR_STORE`
+--
+
+DROP TABLE IF EXISTS `ERROR_STORE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ERROR_STORE` (
+  `id_log` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_source` int(10) unsigned DEFAULT NULL,
+  `archive` varchar(30) DEFAULT NULL,
+  `location` varchar(120) DEFAULT NULL,
+  `reg_type` varchar(50) DEFAULT NULL,
+  `date` varchar(25) DEFAULT NULL,
+  `sequence` varchar(60) DEFAULT NULL,
+  `role` varchar(30) DEFAULT NULL,
+  `guid` varchar(80) DEFAULT NULL,
+  `reg_key` int(10) unsigned DEFAULT NULL,
+  `pers_key` int(10) unsigned DEFAULT NULL,
+  `report_class` varchar(2) DEFAULT NULL,
+  `report_type` int(10) unsigned DEFAULT NULL,
+  `content` varchar(200) DEFAULT NULL,
+  `date_time` datetime NOT NULL,
+  `flag` tinyint(3) DEFAULT NULL,
+  `date_export` varchar(25) DEFAULT NULL,
+  `destination` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_log`),
+  UNIQUE KEY `unique_index` (`id_source`,`location`,`reg_type`,`date`,`sequence`,`role`,`content`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-07 15:05:29
---
+++ LinksProject/src/main/python/export_reports/import_logs.py
@@ -0,0 +1,479 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Author:		Fons Laan, KNAW IISH - International Institute of Social History
+Project:	LINKS
+Name:		import_logs.py
+Version:	0.1
+Goal:		collect error log tables into a single table ERROR_STORE
+Notice:		See the variable x_codes below. If the ref_report table is updated 
+			with new report_type values for 'x' codes, this variable must also 
+			be updated. 
+
+05-Sep-2016 Created
+05-Sep-2016 Changed
+"""
+
+# python-future for Python 2/3 compatibility
+from __future__ import ( absolute_import, division, print_function, unicode_literals )
+from builtins import ( ascii, bytes, chr, dict, filter, hex, input, int, map, next, 
+	oct, open, pow, range, round, str, super, zip )
+
+import os
+import sys
+import datetime
+from time import time
+from dateutil.parser import parse
+import MySQLdb
+
+debug = False
+
+#begin_date_default = "2015-01-01"
+#end_date_default   = "2016-12-31"
+begin_date_default = "2016-04-15"
+end_date_default   = "2016-05-12"
+
+# db
+HOST   = "localhost"
+#HOST   = "10.24.64.154"
+#HOST   = "10.24.64.158"
+
+USER   = "links"
+PASSWD = "mslinks"
+DBNAME = ""				# be explicit in all queries
+
+"""
+HOST_REF   = "10.24.64.30"
+USER_REF   = "hsnref"
+PASSWD_REF = "refhsn"
+DBNAME_REF = ""				# be explicit in all queries
+"""
+HOST_REF   = "localhost"
+USER_REF   = "links"
+PASSWD_REF = "mslinks"
+DBNAME_REF = ""				# be explicit in all queries
+
+single_quote = "'"
+double_quote = '"'
+
+# column 'type' of ref_report, where column 'content' contains 'standard_code= 'x''.
+x_codes = [ 21, 31, 41, 51, 61, 71, 81, 91, 141, 251, 1009, 1109 ]
+
+"""
+# incomplete
+full_archive_names = [ 
+	{ "id_source" : 211, "name" : "" },
+	{ "id_source" : 212, "name" : "" },
+	{ "id_source" : 213, "name" : "" },
+	{ "id_source" : 214, "name" : "" },
+	{ "id_source" : 215, "name" : "" },
+	{ "id_source" : 216, "name" : "" },
+	{ "id_source" : 217, "name" : "" },
+	{ "id_source" : 218, "name" : "" },
+	{ "id_source" : 219, "name" : "219-is-not-used" },
+	{ "id_source" : 220, "name" : "" },
+	{ "id_source" : 221, "name" : "" },
+	{ "id_source" : 222, "name" : "" },
+	{ "id_source" : 223, "name" : "" },
+	{ "id_source" : 224, "name" : "" },
+	{ "id_source" : 225, "name" : "" },
+	{ "id_source" : 226, "name" : "" },
+	{ "id_source" : 227, "name" : "" },
+	{ "id_source" : 228, "name" : "" },
+	{ "id_source" : 229, "name" : "" },
+	{ "id_source" : 230, "name" : "" },
+	{ "id_source" : 231, "name" : "Gemeentearchief Oegstgeest" },
+#	{ "id_source" : 231, "name" : "Gemeentearchief Leidschendam-Voorburg" },
+	{ "id_source" : 232, "name" : "" },
+	{ "id_source" : 233, "name" : "" },
+	{ "id_source" : 234, "name" : "" },
+	{ "id_source" : 235, "name" : "" },
+	{ "id_source" : 236, "name" : "" },
+	{ "id_source" : 237, "name" : "" },
+	{ "id_source" : 238, "name" : "" },
+	{ "id_source" : 239, "name" : "" },
+	{ "id_source" : 240, "name" : "" },
+	{ "id_source" : 241, "name" : "" },
+	{ "id_source" : 242, "name" : "Gemeentearchief Wassenaar" },
+	{ "id_source" : 243, "name" : "Regionaal Archief Leiden" },
+	{ "id_source" : 244, "name" : "" }
+]
+"""
+
+short_archive_names = {
+	"211" : "Groningen",
+	"212" : "Fri_Tresoar",
+	"213" : "Drenthe",
+	"214" : "Overijssel",
+	"215" : "Gelderland",
+	"216" : "Utrecht",
+	"217" : "N-H_Haarlem",
+	"218" : "Z-H_Nat-Archief",
+	"219" : "219-is-not-used",
+	"220" : "NBr_BHIC",
+	"221" : "Limburg",
+	"222" : "Flevoland",
+	"223" : "Z-H_Rotterdam",
+	"224" : "NBr_Breda",
+	"225" : "Zeeland",
+	"226" : "NBr_Eindhoven",
+	"227" : "Utr_Eemland",
+	"228" : "Fri_Leeuwarden",
+	"229" : "N-H_Alkmaar",
+	"230" : "Ned-Antillen",
+	"231" : "Z-H_Oegstgeest",
+	"232" : "Z-H_Dordrecht",
+	"233" : "Z-H_Voorne",
+	"234" : "Z-H_Goeree",
+	"235" : "Z-H_Rijnstreek",
+	"236" : "Z-H_Midden-Holland",
+	"237" : "Z-H_Vlaardingen",
+	"238" : "Z-H_Midden",
+	"239" : "Z-H_Gorinchem",
+	"240" : "Z-H_Westland",
+	"241" : "Z-H_Leidschendam",
+	"242" : "Z-H_Wassenaar",
+	"243" : "Z-H_Leiden",
+	"244" : "Z-H_Delft"
+}
+
+archive_names = short_archive_names
+
+
+	
+class Database:
+	def __init__( self, host, user, passwd, dbname ):
+		self.host   = host
+		self.user   = user
+		self.passwd = passwd
+		self.dbname = dbname
+
+		self.connection = MySQLdb.connect( \
+			host = self.host, 
+			user = self.user, 
+			passwd = self.passwd, 
+			db = self.dbname,
+			charset     = "utf8",			# needed when there is e.g. 
+			use_unicode = True				# &...; html escape stuff in strings
+		)
+		self.cursor = self.connection.cursor()
+
+	def insert( self, query ):
+		try:
+			resp = self.cursor.execute( query )
+			self.connection.commit()
+		except:
+			self.connection.rollback()
+			etype = sys.exc_info()[ 0:1 ]
+			value = sys.exc_info()[ 1:2 ]
+			log.write( "%s, %s\n" % ( etype, value ) )
+			exit( 1 )
+
+	def query( self, query ):
+	#	print( "\n%s" % query )
+		cursor = self.connection.cursor( MySQLdb.cursors.DictCursor )
+		cursor.execute( query )
+		return cursor.fetchall()
+
+	def info( self ):
+		"""
+		See the MySQLdb User's Guide: 
+		Returns some information about the last query. Normally you don't need to check this. If there are any MySQL 
+		warnings, it will cause a Warning to be issued through the Python warning module. By default, Warning causes 
+		a message to appear on the console. However, it is possible to filter these out or cause Warning to be raised 
+		as exception. See the MySQL docs for mysql_info(), and the Python warning module. (Non-standard)
+		"""
+		return self.connection.info()
+
+	def __del__( self ):
+		self.connection.close()
+
+
+
+def db_check( db ):
+	if debug: print( "db_check()" )
+
+	# links_a2a
+	#tables = [ "a2a", "event", "object", "person", "person_o_temp", "person_profession", 
+	#	"registration_o_temp", "relation", "remark", "source", "source_sourceavailablescans_scan" ]
+	
+	db_name = "links_match"
+	tables = [ "match_process", "match_view", "matches", "matrix", "notation" ]
+
+	print( "table row counts:" )
+	for table in tables:
+		query = """SELECT COUNT(*) FROM %s.%s""" % ( db_name, table )
+		resp = db.query( query )
+		if resp is not None:
+			count_dict = resp[ 0 ]
+			count = count_dict[ "COUNT(*)" ]
+			print( "%s %d" % ( table, count ) )
+		else:
+			print( "Null response from db" )
+
+	# we could show the strings from these GROUPs for cheking, because there should be no variation
+	# SELECT eventtype, COUNT(*) FROM links_a2a.event GROUP BY eventtype;
+	# SELECT relationtype, COUNT(*) FROM links_a2a.relation GROUP BY relationtype;
+	# SELECT relation, COUNT(*) FROM links_a2a.relation GROUP BY relation;
+	# SELECT remark_key, COUNT(*) FROM links_a2a.remark GROUP BY remark_key;
+
+
+
+def get_log_names( db_logs ):
+	if debug: print( "get_log_names()" )
+	
+	log_names = []
+	
+	query = "USE links_logs;"
+	#print( query )
+	resp = db_logs.query( query )
+	if resp is None:
+		print( "Null response from db_logs" )
+		return log_names
+
+	query = "SHOW TABLES;"
+	#print( query )
+	resp = db_logs.query( query )
+	if resp is not None:
+		ntables = len( resp )
+		for t in range( ntables ):
+			resp_t = resp[ t ]
+			#print( resp[ t ] )
+			table_name = resp_t[ "Tables_in_links_logs" ]
+			#print( table_name )
+			log_names.append( table_name )
+	else:
+		print( "Null response from db_logs" )
+
+	return log_names
+
+
+
+def inspect_log( n, log_name ):
+	accept = False
+	
+	query = "SELECT COUNT(*) AS count FROM links_logs.`%s`;" % log_name
+	#print( query )
+	resp = db_logs.query( query )
+	if resp is not None:
+		#print( resp )
+		count = resp[ 0 ][ "count" ]
+		#print( count )
+		if count == 0:
+			print( "%3d: %s count = %d, skip" % ( n, log_name, count ) )
+			return
+	
+	"""
+	query = "SELECT id_source, COUNT(*) AS count FROM links_logs.`%s` GROUP BY id_source;" % log_name
+	#print( query )
+	resp = db_logs.query( query )
+	if resp is not None:
+		ndict = len( resp )
+		#print( ndict, resp )
+		print( "%3d: %s inspect" % ( n, log_name ) )
+		for t in range( ndict ):
+			count = resp[ t ][ "count" ]
+			id_source = resp[ t ][ "id_source" ]
+			print( "     number of log records for id_source %3d is %s" % ( id_source, count ) )
+
+		msg = "%3d: %s include in report? [Y,n]" % ( n, log_name )
+		answer = input( msg )
+		if answer == '' or answer == 'Y' or answer == 'y':
+			accept = True
+	"""
+	
+	# only skip 0 counts, accept the rest
+	print( "%3d: %s count = %d, accept" % ( n, log_name, count ) )
+	accept = True
+	
+	return accept
+
+
+
+def select_log_names( db_logs, begin_date, end_date ):
+	if debug: print( "select_log_names()" )
+	all_names = get_log_names( db_logs )
+	log_names = []	# accepted log names
+	nnames = len( all_names )
+	
+	for n in range( nnames ):
+		log_name = all_names[ n ]
+		if not log_name.startswith( "log-" ):
+			print( "%3d: %s not a log table we want, skip" % ( n, log_name ) )
+			continue	# skip this one
+	
+		log_date_str = log_name[ 4:14 ]
+		#print( log_date_str )
+	
+		log_date = validate_date( log_date_str )
+		if log_date is None:
+			continue	# skip this one
+		
+		if log_date < begin_date:
+			print( "%3d: %s too old, skip" % ( n, log_name ) )
+		elif log_date > end_date:
+			print( "%3d: %s too new, skip" % ( n, log_name ) )
+		else:
+			#print( "%3d: %s inspect" % ( n, log_name ) )
+			accept = inspect_log( n, log_name )
+			if accept:
+				log_names.append( log_name )
+			
+	return log_names
+
+
+
+def none2empty( var ):
+	if var is None or var == "None":
+		var = ""
+	return var
+
+
+
+def process_logs( log_names ):
+	if debug: print( "rocess_logs()" )
+	print( "\nprocessing selected logs" )
+	nnames = len( log_names )
+	fields = "id_log, id_source, archive, location, reg_type, date, sequence, role, guid, "
+	fields += "reg_key, pers_key, report_class, report_type, content, date_time"
+	
+	clause = ""
+	for xtype in x_codes:
+		if clause == "":
+			clause = "report_type = %d" % xtype
+		else:
+			clause += " OR report_type = %d" % xtype
+	#print( clause )
+	
+	for n in range( nnames ):
+		log_name = log_names[ n ]
+		print( log_name )
+			
+		query = "SELECT %s FROM links_logs.`%s` WHERE NOT (%s);" % ( fields, log_name, clause )
+		if debug: print( query )
+		
+		resp = db_logs.query( query )
+		if resp is not None:
+			nrec = len( resp )
+			print( "number of records: %d" %nrec )
+			for r in range( nrec ):
+				rec = resp[ r ]
+				if debug: print( "record %d-of-%d" % ( r+1, nrec ) )
+				if debug: print( rec )
+				
+				id_log       = none2empty( rec[ "id_log" ] )
+				id_source    = none2empty( rec[ "id_source" ] )
+				archive      = none2empty( rec[ "archive" ] )
+				location     = none2empty( rec[ "location" ] )
+				reg_type     = none2empty( rec[ "reg_type" ] )
+				date         = none2empty( rec[ "date" ] )
+				sequence     = none2empty( rec[ "sequence" ] )
+				role         = none2empty( rec[ "role" ] )
+				guid         = none2empty( rec[ "guid" ] )
+				reg_key      = none2empty( rec[ "reg_key" ] )
+				pers_key     = none2empty( rec[ "pers_key" ] )
+				report_class = none2empty( rec[ "report_class" ] )
+				report_type  = none2empty( rec[ "report_type" ] )
+				content      = none2empty( rec[ "content" ] )
+				date_time    = str( none2empty( rec[ "date_time" ] ) )
+
+				if debug:
+					print( "id_log       = %s" % id_log )
+					print( "id_source    = %s" % id_source  )
+					print( "archive      = %s" % archive )
+					print( "location     = %s" % location )
+					print( "reg_type     = %s" % reg_type )
+					print( "date         = %s" % date )
+					print( "sequence     = %s" % sequence )
+					print( "role         = %s" % role )
+					print( "guid         = %s" % guid )
+					print( "reg_key      = %s" % reg_key )
+					print( "pers_key     = %s" % pers_key )
+					print( "report_class = %s" % report_class )
+					print( "report_type  = %s" % report_type )
+					print( "content      = %s" % content )
+					print( "date_time    = %s" % date_time )
+				
+				# without id_log (also pk of ERROR_STORE table)
+				er_fields = fields[ 8: ]
+				er_values = ( id_source, archive, location, reg_type, date, sequence, role, guid, 
+					reg_key, pers_key, report_class, report_type, content, date_time )
+				
+				# Using 'IGNORE' to ignore violations of the unique_index constraint
+				er_query = "INSERT IGNORE INTO links_logs.ERROR_STORE ( %s ) VALUES %s;" % ( er_fields, er_values )
+				if debug: print ( er_query )
+				er_resp = db_logs.insert( er_query )
+				if er_resp is not None:
+					print( "er_resp:", er_resp )
+				
+				#input( "<Enter> to continue" )
+				
+
+def validate_date( date_string ):
+	date = None
+	
+	try: 
+		date = parse( date_string )
+	except ValueError:
+		print( "validate_date() ValueError: %s" % date_string )
+
+	return date
+
+
+
+def get_date_limit( prompt, date_default ):
+	print( prompt )
+	date_str = input( "yyyy-mm-dd (%s) " % date_default )
+	if date_str == "":
+		date_str = date_default
+	print( "date: %s" % date_str )
+	
+	date = validate_date( date_str )
+	if date is None:
+		print( "%s date is not a valid date" % date )
+	
+	return date
+
+
+
+def get_date_limits():
+	prompt = "Begin date for inclusion of log tables: "
+	begin_date = get_date_limit( prompt, begin_date_default )
+	
+	if begin_date is not None:
+		prompt = "End date for inclusion of log tables: "
+		end_date = get_date_limit( prompt, end_date_default )
+	
+	if begin_date > end_date:
+		print( "Begin date exceeds end date" )
+		begin_date = end_date = None
+		
+	return begin_date, end_date
+
+
+
+if __name__ == "__main__":
+#	print( "links host db: %s \tfor frequency and levenshtein tables" % HOST )
+#	print( "reference db:  %s \tfor reference tables" % HOST_REF )
+
+	print( "export_reports.py" )
+
+	begin_date, end_date = get_date_limits()
+	if begin_date is None or end_date is None:
+		print( "EXIT" )
+		sys.exit( 1 )
+	
+	db = Database( host = HOST, user = USER, passwd = PASSWD, dbname = DBNAME )
+
+	db_ref = Database( host = HOST_REF, user = USER_REF, passwd = PASSWD_REF, dbname = DBNAME_REF )
+	
+	db_logs = Database( host = HOST, user = USER, passwd = PASSWD, dbname = DBNAME )
+	
+#	db_check( db )
+
+	log_names = select_log_names( db_logs, begin_date, end_date )
+	process_logs( log_names )
+	
+
+# [eof]
