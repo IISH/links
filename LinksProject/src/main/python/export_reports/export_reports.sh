d1cf2170d4ae8a8ab596727e04aae35f7254d0cb
Fons Laan <fons.laan@gmail.com>
2016-09-13 17:26:47 +0200

    more export reports

--- LinksProject/src/main/python/export_reports/export_reports.sh
+++ LinksProject/src/main/python/export_reports/export_reports.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# FL-07-Sep-2016
+# FL-13-Sep-2016
 # export_reports.sh
 
 # activate the virtual python
-source "../../python2712/bin/activate"
+#source "../../python2712/bin/activate"
+source "../../python352/bin/activate"
 
 # start the python import script
 python export_reports.py
