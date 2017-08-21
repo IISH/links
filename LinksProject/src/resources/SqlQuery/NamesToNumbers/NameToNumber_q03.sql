25cb1ebd034542c6397f21f3572815b8bbe7d867
Fons Laan <fons.laan@gmail.com>
2017-08-21 16:10:00 +0200

    queries in java source

--- LinksProject/src/resources/SqlQuery/NamesToNumbers/NameToNumber_q03.sql
+++
@@ -1,4 +0,0 @@
--- Query 03
-UPDATE links_cleaned.person_c, links_prematch.freq_firstname 
-SET firstname2_no = links_prematch.freq_firstname.id 
-WHERE links_cleaned.person_c.firstname2 = links_prematch.freq_firstname.name_str ;
--- LinksProject/src/resources/SqlQuery/NamesToNumbers/NameToNumber_q05.sql
+++
@@ -1,4 +0,0 @@
--- Query 05
-UPDATE links_cleaned.person_c, links_prematch.freq_firstname 
-SET firstname4_no = links_prematch.freq_firstname.id 
-WHERE links_cleaned.person_c.firstname4 = links_prematch.freq_firstname.name_str ;
